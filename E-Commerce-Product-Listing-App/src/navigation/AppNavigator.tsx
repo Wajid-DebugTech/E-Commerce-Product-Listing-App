e4de793e782f9fad9ea4311ea80eb7d3c1153158
shiraz <shirazacssd@gmail.com>
2026-09-19 12:27:12 -0700

    navigation skeleton

---
+++ E-Commerce-Product-Listing-App/src/navigation/AppNavigator.tsx
@@ -0,0 +1,20 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types/navigation";
+import Home from "../screens/Home";
+import ProductDetails from "../screens/ProductDetails";
+import Cart from "../screens/Cart";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ProductDetails" component={ProductDetails} />
+        <Stack.Screen name="Cart" component={Cart} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ E-Commerce-Product-Listing-App/src/screens/ProductDetails.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ProductDetails() {
+  return (
+    <View>
+      <Text>Product Details</Text>
+    </View>
+  );
+}
