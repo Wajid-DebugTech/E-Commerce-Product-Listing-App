d20a7a941a1ec6c393468b7212182460cbde5222
shiraz <shirazacssd@gmail.com>
2026-09-19 16:46:49 -0700

    header complete (minus button interaction)

--- E-Commerce-Product-Listing-App/src/components/Header.tsx
+++ E-Commerce-Product-Listing-App/src/components/Header.tsx
@@ -1,7 +1,61 @@
-import { View } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default function Header() {
-    return (
-        <View style={{backgroundColor: "#f00", paddingVertical: 70}} />
-    )
+interface HeaderProps {
+  onSearchPress?: () => void;
+  onCartPress?: () => void;
 }
+
+export default function Header({ onSearchPress, onCartPress }: HeaderProps) {
+  return (
+    <View
+      style={{
+        backgroundColor: "#eee",
+        borderBottomWidth: 1,
+        borderBottomColor: "#ccc",
+        paddingTop: useSafeAreaInsets().top + 10,
+        paddingBottom: 10,
+        flexDirection: "row",
+        paddingHorizontal: 20,
+      }}
+    >
+      <TouchableOpacity style={styles.button} onPress={onSearchPress}>
+        <Ionicons name="search" size={36} color="#fff" />
+      </TouchableOpacity>
+
+      <View style={{ flex: 1 }}>
+        <Text style={styles.title}>BuyStuff</Text>
+        <Text style={styles.subtitle}>Buy Good Stuff from BuyStuff</Text>
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={onCartPress}>
+        <Ionicons name="cart" size={36} color="#fff" />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 36,
+    color: "#000",
+    textAlign: "center",
+    fontWeight: "800",
+    marginTop: 0,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: "#666",
+    textAlign: "center",
+    fontWeight: "600",
+  },
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 20,
+    backgroundColor: "#8a4bff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
